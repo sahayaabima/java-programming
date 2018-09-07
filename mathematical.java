41ea7d21e54eafdea9d62921743b9baf81a814f4
sahayaabima <43032775+sahayaabima@users.noreply.github.com>
2018-09-07 10:53:05 +0530

    Add files via upload

---
+++ mathematical.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    int a=5,b=5,c;
+    c=a+b;
+    System.out.println("Add the two numbers"+c);
+    c=a-b;
+    System.out.println("subract the two numbers"+c);
+    c=a*b;
+    System.out.println("multiplication the two numbers"+c);
+    c=a/b;
+    System.out.println("division of the two numbers"+c);
+  }
+}
