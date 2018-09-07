e72fb1b6a9213bc999ad0a82361ebddd5de10afd
sahayaabima <43032775+sahayaabima@users.noreply.github.com>
2018-09-07 11:43:45 +0530

    Add files via upload

---
+++ floating.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    int a=3;
+    float b=4,c;
+    c=a+b;
+    System.out.println("add the two numbers:"+c);
+    c=a-b;
+    System.out.println("subtract the two numbers:"+c);
+    c=a*b;
+    System.out.println("multiply the two numbers:"+c);
+    c=a/b;
+    System.out.println("divide the two numbers:"+c);
+  }
+}
