1455b3b845a63a74c82cb60bd4e5d64b04f2113f
sahayaabima <43032775+sahayaabima@users.noreply.github.com>
2018-09-14 15:11:37 +0530

    Add files via upload

---
+++ javadatatypes.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    String name = "Abima";
+    System.out.println("Name:"+name);
+    String city = "Nagercoil";
+    System.out.println("City:"+city);
+    float height = 4;
+    System.out.println("Height:"+height);
+    byte age = 20;
+    System.out.println("Age:"+age);
+    boolean married = true;
+    System.out.println("Married:"+married);
+    char sex = 'F';
+    System.out.println("Sex:"+sex);
+    double citypopulation = 4.5634345;
+    System.out.println("City population:"+citypopulation);
+  }
+}
