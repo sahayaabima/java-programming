6ae62dde7f2b948878c8ebf0e3e079306fdace3f
sahayaabima <43032775+sahayaabima@users.noreply.github.com>
2018-09-18 14:55:08 +0530

    Add files via upload

---
+++ ifstatement.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+  int daysintheatre=50;
+     if( daysintheatre < 50 ){
+	System.out.println("daysintheatre is less than 50");
+     }
+     else {
+	System.out.println("daysintheatre is greater than or equal 50");
+     }
+  }
+}
