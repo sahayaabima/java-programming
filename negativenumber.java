e0443514f424dbeacbad491f8fcdc92b185bb789
sahayaabima <43032775+sahayaabima@users.noreply.github.com>
2018-10-03 12:00:49 +0530

    Add files via upload

---
+++ negativenumber.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+  int number=-12;
+    if(number > 0)
+    {
+     System.out.println("positive number");
+    }
+    else if(number < 0)
+    {
+      System.out.println("negative number");
+    }
+    else
+    {
+      System.out.println("zero");
+    }
+  }
+}
