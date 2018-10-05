d3998f9c9b3e97a0a424c1f4f7828a9e24248b4c
sahayaabima <43032775+sahayaabima@users.noreply.github.com>
2018-10-05 12:16:20 +0530

    Add files via upload

---
+++ naturalnum.java
@@ -0,0 +1,10 @@
+class naturalnum {
+  public static void main(String[] args) {
+  int num = 10, count, total = 0;
+     for(count = 1; count <= num; count++){
+           total = total + count;
+       }
+
+       System.out.println(total);
+  }
+}
